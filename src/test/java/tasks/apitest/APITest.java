2ed6c1fa9a8dffaca360bbfd09cc1395566a6598
Lyene <lyene.benvenutti@southsystem.com.br>
2023-01-13 11:56:02 -0300

    first commit

---
+++ src/test/java/tasks/apitest/APITest.java
@@ -0,0 +1,51 @@
+package tasks.apitest;
+
+import io.restassured.http.ContentType;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.is;
+
+public class APITest {
+
+    @BeforeClass
+    public static void setup() {
+        baseURI = "http://localhost:8001/tasks-backend";
+    }
+
+    @Test
+    public void deveRetornarTasks() {
+        given()
+        .when()
+            .get("/todo")
+        .then()
+            .statusCode(200)
+        ;
+    }
+
+    @Test
+    public void deveAdicionarTaskComSucesso() {
+        given()
+            .body("{ \"task\": \"Teste via API\", \"dueDate\": \"2030-12-30\" }")
+            .contentType(ContentType.JSON)
+        .when()
+            .post("/todo")
+        .then()
+            .statusCode(201)
+        ;
+    }
+
+    @Test
+    public void naoDeveAdicionarTaskInvalida() {
+        given()
+            .body("{ \"task\": \"Teste via API\", \"dueDate\": \"2010-12-30\" }")
+            .contentType(ContentType.JSON)
+        .when()
+            .post("/todo")
+        .then()
+            .statusCode(400)
+            .body("message", is("Due date must not be in past"))
+        ;
+    }
+}
